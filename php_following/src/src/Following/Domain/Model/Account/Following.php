38a2d9e340e2c63a11b14daa334f2a58865addd8
adrianet86 <adria.matoses@gmail.com>
2019-07-22 01:50:12 +0200

    Following classes

---
+++ php_following/src/src/Following/Domain/Model/Account/Following.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Following\Domain\Model\Account;
+
+
+interface Following
+{
+    public function followAccount(Account $account): void;
+}
---
+++ php_following/src/src/Following/Infrastructure/Api/Account/InstagramFollowingAdapter.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Following\Infrastructure\Api\Account;
+
+
+use App\Following\Domain\Model\Account\Account;
+use App\Following\Domain\Model\Account\Following;
+use InstagramAPI\Instagram;
+
+class InstagramFollowingAdapter implements Following
+{
+    const WAIT = 2;
+
+    private Instagram $ig;
+    private string $username;
+    private string $password;
+
+    public function __construct(
+        array $config,
+        bool $debug,
+        bool $truncatedDebug,
+        string $username,
+        string $password
+    )
+    {
+        $this->ig = new Instagram(
+            $debug,
+            $truncatedDebug,
+            $config
+        );
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function followAccount(Account $account): void
+    {
+        $this->ig->login($this->username, $this->password);
+        $this->ig->people->follow($account->instagramId());
+        $account->setFollowingRequestedAt();
+    }
+}
